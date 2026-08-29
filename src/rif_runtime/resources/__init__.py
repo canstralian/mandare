08aacd4d3aeb56fad99d4694ee6fdd5a827e070a
WhacktheJacker <8595080+canstralian@users.noreply.github.com>
2026-08-29 22:02:34 +1000

    feat: expose resource discovery contracts

--- src/rif_runtime/resources/__init__.py
+++ src/rif_runtime/resources/__init__.py
@@ -10,6 +10,14 @@ from .descriptor import (
     ResourceCapabilityDescriptor,
     ResourceEffect,
 )
+from .discovery import (
+    ARDDiscoveryClient,
+    DiscoveryIntent,
+    ResourceCandidate,
+    ResourceDiscovery,
+    ResourceDiscoveryError,
+    StaticResourceDiscovery,
+)
 from .exceptions import (
     DuplicateResourceCapabilityError,
     ResourceError,
@@ -33,20 +41,27 @@ from .scanner import RepositoryScanner
 from .snapshot import ResourceSnapshot
 
 __all__ = [
+    "ARDDiscoveryClient",
+    "DiscoveryIntent",
     "DuplicateResourceCapabilityError",
     "ModuleInfo",
     "RepositoryResource",
+    "RepositoryCandidate",
     "RepositoryScanner",
     "RepositorySnapshot",
     "RepositorySnapshotBuilder",
     "ResourceCapabilityDescriptor",
     "ResourceCapabilityRegistry",
+    "ResourceCandidate",
+    "ResourceDiscovery",
+    "ResourceDiscoveryError",
     "ResourceEffect",
     "ResourceError",
     "ResourceId",
     "ResourceKind",
     "ResourceReference",
     "ResourceSnapshot",
+    "StaticResourceDiscovery",
     "TestInfo",
     "UnknownResourceCapabilityError",
 ]
